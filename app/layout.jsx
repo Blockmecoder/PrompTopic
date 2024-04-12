339aae3893638e0c09e253ca77e9282a8c68364c
HARSHIT <apsbqt@gmail.com>
2024-04-12 08:15:27 +0530

    data

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import Nav from "@components/Nav";
+import "@styles/globals.css";
+import { Children } from "react";
+
+export const Metadata = {
+  title: "Promptopia",
+  description: "Discover and Share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav/>
+          {children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
